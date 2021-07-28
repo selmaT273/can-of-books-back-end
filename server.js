60847d028c5e7652c8ab37f01db92b21fb4fd482
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-28 13:44:28 -0500

    resolved error installing mongoose and verified connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -8,8 +9,17 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+mongoose.connect('mongodb://localhost:27017/canofbooks', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongoose is connected!!!!');
+})
+
 app.get('/', (request, response) => {
   response.send('success!');
 });
+
 
 app.listen(PORT, () => console.log(`PORT is listening on ${PORT}`));
